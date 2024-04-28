8f242dad758e04f3254e5185ff107f3be4ed60d9
n3lly90 <155855578+n3lly90@users.noreply.github.com>
2024-04-28 19:16:58 +0100

    Update age-quiz.py

--- age-quiz.py
+++ age-quiz.py
@@ -1,14 +1,19 @@
-# Practical Task 
+# Prompt the user to input their age
 age = int(input("Please enter your age: \n"))
 
-# Use of If- Elif - Else function to print desired outcome
+# Use if-elif-else statements to determine the appropriate message based on the age entered
 if age > 100:
-        print("Sorry, you're dead.")
+    # If age is greater than 100, print a message indicating the person is considered deceased
+    print("Sorry, you're dead.")
 elif age >= 65:
-        print("Enjoy your retirement! \n")
+    # If age is 65 or greater, print a message indicating the person is eligible for retirement
+    print("Enjoy your retirement! \n")
 elif age < 13:
-        print("You qualify for the kiddie discount! \n")
+    # If age is less than 13, print a message indicating the person qualifies for a discount typically given to children
+    print("You qualify for the kiddie discount! \n")
 elif age == 21:
-        print("Congrats on your 21st! \n")
+    # If age is exactly 21, print a message to celebrate the person's 21st birthday
+    print("Congrats on your 21st! \n")
 else:
-        print("Age is but a number. \n")
+    # For any other age, print a generic message
+    print("Age is but a number. \n")
